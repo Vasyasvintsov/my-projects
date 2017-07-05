e491c7f5591202784db48b428a456b034c4512da
Vasyasvintsov <basilsvintsov@gmail.com>
2017-07-05 21:52:53 +0300

    test git home

--- my_http_server.py
+++ my_http_server.py
@@ -1,19 +1,23 @@
 import socket
 my_socket = socket.socket()
-my_socket.bind(('192.168.0.33', 1966))
-my_socket.listen(1)
-# conn = 0
-conn, addr = my_socket.accept()
+my_socket.bind(('192.168.1.67', 1999))
 
-print 'connected:', addr
+# conn = 0
+# conn, addr = my_socket.accept()
+
+# print 'connected:', addr
 
 # if (conn != 0):
 #     conn.send('HTTP/1.0 200 OK \n\n')
-
+my_socket.listen(2)
 while 1:
-    data = conn.recv(1024)
+
+    conn, addr = my_socket.accept()
+    print 'connected:', addr
+    data = conn.recv(16384)
     if not data:
         break
     conn.send(data)
+    conn.close()
 
-conn.close()
+my_socket.close()
